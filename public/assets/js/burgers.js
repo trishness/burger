4483ede8c925bcd1974dff6e76390415dc13124f
Trish Ness <trishnss@gmail.com>
2020-07-20 06:51:15 -0700

    buttons working, info displaying, styling added

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,55 +1,50 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-devoured").on("click", function(event) {
-      var id = $(this).data("id");
-      var newDevoured = $(this).data("newdevoured");
-  
-      var newDevouredState = {
-        devoured: newDevoured
-      };
-  
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newDevouredState
-      }).then(
-        function() {
-          console.log("changed devoured to", newDevoured);
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#burger").val().trim(),
-        devoured: $("[name=devoured]:checked").val().trim()
-      };
-  
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new burger");
-          location.reload();
-        }
-      );
-    });
-  
-    // $(".delete-cat").on("click", function(event) {
-    //   var id = $(this).data("id");
-  
-    //   // Send the DELETE request.
-    //   $.ajax("/api/cats/" + id, {
-    //     type: "DELETE"
-    //   }).then(
-    //     function() {
-    //       console.log("deleted cat", id);
-    //       // Reload the page to get the updated list
-    //       location.reload();
-    //     }
-    //   );
-    // });
+  $(".change-devoured").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevoured = $(this).data("newdevoured");
+    var newDevouredState = {
+      devoured: newDevoured
+    };
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevoured);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
   });
-  
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    var newBurger = {
+      burger_name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new Burger");
+        location.reload();
+      }
+    );
+  });
+
+//   $(".delete-burger").on("click", function(event) {
+//     var id = $(this).data("id");
+//     $.ajax("/api/burgers/" + id, {
+//       type: "DELETE"
+//     }).then(
+//       function() {
+//         console.log("deleted burger", id);
+//         location.reload();
+//       }
+//     );
+//   });
+// });
