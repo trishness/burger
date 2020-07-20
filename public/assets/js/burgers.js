d5dd5849cdc0cf3acea82f4798d71374bf0967ca
Trish Ness <trishnss@gmail.com>
2020-07-19 18:02:01 -0700

    main and ajax set up

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,55 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newSleep = $(this).data("newdevoured");
+  
+      var newDevouredState = {
+        devoured: newDevoured
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burger").val().trim(),
+        sleepy: $("[name=devoured]:checked").val().trim()
+      };
+  
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          location.reload();
+        }
+      );
+    });
+  
+    // $(".delete-cat").on("click", function(event) {
+    //   var id = $(this).data("id");
+  
+    //   // Send the DELETE request.
+    //   $.ajax("/api/cats/" + id, {
+    //     type: "DELETE"
+    //   }).then(
+    //     function() {
+    //       console.log("deleted cat", id);
+    //       // Reload the page to get the updated list
+    //       location.reload();
+    //     }
+    //   );
+    // });
+  });
+  
