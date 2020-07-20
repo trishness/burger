4ad11988e328d1cbc55a728151148842fb350cea
Trish Ness <trishnss@gmail.com>
2020-07-19 17:36:26 -0700

    orm and burger file set up

---
+++ config/orm.js
@@ -0,0 +1,89 @@
+var connection = require("../config/connection.js");
+
+function printQuestionMarks(number) {
+    var array = [];
+
+    for (var i=0; i<number; i++) {
+        array.push("?");
+    }
+    return array.toString();
+}
+
+function objectToSql(object) {
+    var array = [];
+
+    for (var key in object) {
+        var value=object[key];
+
+        if (Object.hasOwnProperty.call(object, key)) {
+            if (typeof value === "string" && value.indexOf(" ")>=0) {
+                value = "'" + value + "'";
+            }
+            array.push(key + "=" + value);
+        }
+    }
+    return array.toString();
+}
+
+var orm = {
+    all: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result)
+        });
+    },
+    create: function(table, cols, vals, cb){
+        var queryString = "INSERT INTO " + table;
+
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+
+        console.log(queryString);
+
+        connection.query(queryString, vals, function(err, result){
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: function(table,objColVals, condition, cb){
+        var queryString = "UPDATE " + table;
+
+        queryString += " SET ";
+        queryString += objectToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
+        console.log(queryString);
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    delete: function(table, condition, cb){
+        var queryString = "DELETE FROM " + table;
+
+        queryString += " WHERE ";
+        queryString += condition;
+
+        connection.query(queryString, function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+};
+
+module.exports = orm;
---
+++ db/models/burger.js
@@ -0,0 +1,21 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(result){
+            cb(result);
+        });
+    },
+    insertOne: function(cols,vals,cb){
+        orm.insertOne("burgers", cols, vals, function(result){
+            cb(result);
+        });
+    },
+    updateOne: function(objColVals, condition, cb){
+        orm.updateOne("burgers", objColVals, condition, function(result){
+            cb(result);
+        });
+    }
+};
+
+module.exports = burger;
